8f6e60a93d556d8f8b626947ff9fabc4d282a8ba
wanniDev <wannidev0928@gmail.com>
2021-08-30 17:44:43 +0900

    init project

---
+++ src/main/java/com/project/doubleshop/DoubleShopApplication.java
@@ -0,0 +1,13 @@
+package com.project.doubleshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoubleShopApplication.class, args);
+    }
+
+}
