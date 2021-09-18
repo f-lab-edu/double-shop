0e7df62f3f2e49d97dc1777adc97a94fe375c1f3
wanniDev <wannidev0928@gmail.com>
2021-09-18 14:49:32 +0900

    feat : 테스트용 마이바티스 설정 xml 적용 및 간단한 학습 테스트 추가, 그리고 불필요한 코드 삭제.

--- src/test/java/com/project/doubleshop/ArticleMapper.java
+++ src/test/java/com/project/doubleshop/ArticleMapper.java
@@ -1,9 +1,10 @@
 package com.project.doubleshop;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
+@Mapper
 public interface ArticleMapper {
-    @Select("SELECT * FROM ARTICLES WHERE id = #{id}")
-    Article getArticle(@Param("id") Long id);
+    Article selectById(Long id);
 }
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,5 +1,2 @@
 INSERT INTO ARTICLES (title, author)
 VALUES ('Working with MyBatis in Spring', 'Baeldung');
-
-INSERT INTO GALLERY (title, author)
-VALUES ('Working with MyBatis in Spring', 'Baeldung');
