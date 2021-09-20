487468b911d671c94877fbbeb96f202c05c39c1d
wanniDev <wannidev0928@gmail.com>
2021-09-20 15:31:01 +0900

    사용하지 않는 쿼리 및 테스트, 매핑 삭제

--- src/test/java/com/project/doubleshop/test/domain/mapper/ArticleMapper.java
+++
@@ -1,9 +0,0 @@
-package com.project.doubleshop.test.domain.mapper;
-
-import com.project.doubleshop.test.domain.entity.Article;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ArticleMapper {
-    Article selectById(Long id);
-}
