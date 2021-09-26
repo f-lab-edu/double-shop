20ef1ce6d0bd7c34e197d82c341b4ff19d53d2f3
Daniel <caletabuena33@gmail.com>
2021-09-26 22:37:26 +0900

    feat: 회원 및 로그인 관련 DTO 추가
    - MemberInfoDto 정의
    - LogInRequestDto 정의

---
+++ src/main/java/com/project/doubleshop/web/member/dto/LogInRequestDto.java
@@ -0,0 +1,19 @@
+package com.project.doubleshop.web.member.dto;
+
+import com.project.doubleshop.domain.utils.SHA256EncryptionUtil;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LogInRequestDto {
+
+    private String userId;
+
+    private String password;
+
+    public void encryptPassword(SHA256EncryptionUtil encryptionUtil) {
+        this.password = encryptionUtil.encrypt(password);
+    }
+
+}
