7a02becca38a2dc5f1e0ba5e4fed2a7b845e41b9
wanniDev <wannidev0928@gmail.com>
2021-09-27 14:45:47 +0900

    Feat: 상품 도메인 v2 추가
     기존 v1에서는 Item을 대카테고리 이름을 기준으로 자식 객체들을 따로 만들었는데,
    관리하기 어려움도 있었을 뿐만 아니라, 아이템 도메인이 카테고리 도메인의 관심사에
    관여하는 듯한 형태의 코드가 만들어졌다.
     그래서 도메인의 관심사를 하나로 응집시킬 필요를 느껴, 이번엔 Item 내부에서
    스스로 인스턴스를 생성하는 구조로 바꾸었고, setter 보다는 빌더패턴으로
    인스턴스를 생성하도록 유도하는 코드를 작성하였다.

---
+++ src/main/java/com/project/doubleshop/web/item/dto/ItemFormDTO.java
@@ -0,0 +1,85 @@
+package com.project.doubleshop.web.item.dto;
+
+import java.time.LocalDate;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ItemFormDTO {
+	// 상품 pk
+	private Long id;
+
+	// 상품 이름
+	private String name;
+
+	// 상품 한줄설명
+	private String description;
+
+	// 브랜드 명
+	private String brandName;
+
+	// 상품 가격
+	private Integer price;
+
+	// 단위
+	private String unit;
+
+	// 용량
+	private String volume;
+
+	// 치수
+	private String dimension;
+
+	// 포장타입
+	private String packageType;
+
+	// 원산지
+	private String origin;
+
+	// 유통기한
+	private String expiration;
+
+	// 100g 당 가격
+	private Integer pricePer100g;
+
+	// 알러지 정보
+	private String allergicInfo;
+
+	// 상품 번호
+	private String modelSerialNo;
+
+	// 상품 평가 점수
+	private Integer rating;
+
+	// 검색 키워드
+	private String searchKeyword;
+
+	// 총 재고수량
+	private Integer stock;
+
+	// 할인가
+	private Integer discountPrice;
+
+	// 작가
+	private String author;
+
+	// 출판사
+	private String publisher;
+
+	// ISBN
+	private String isbn;
+
+	// 발행일
+	private LocalDate publishedTime;
+
+	// 하루배송가능여부
+	private boolean onedayEligible;
+
+	// 당일배송가능여부
+	private boolean freshEligible;
+}
