9e700e7738d2155959d6b92c420b9b996c7d2886
wanniDev <wannidev0928@gmail.com>
2021-09-27 18:24:11 +0900

    Test: 테스트용 상품 schema 컬럼명 변경 및 테스트용 매퍼 파일 이름 변경

--- src/test/java/com/project/doubleshop/DoubleShopApplicationTests.java
+++ src/test/java/com/project/doubleshop/DoubleShopApplicationTests.java
@@ -1,7 +1,7 @@
 package com.project.doubleshop;
 
 import com.project.doubleshop.test.domain.entity.Item;
-import com.project.doubleshop.test.domain.mapper.ItemMapper;
+import com.project.doubleshop.test.domain.mapper.TestItemMapper;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,13 +17,13 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class DoubleShopApplicationTests {
 
     @Autowired
-    ItemMapper itemMapper;
+    TestItemMapper testItemMapper;
 
     @Test
     @Order(1)
     @DisplayName("select 단건 테스트: item pk 1번에는 item 데이터가 존재한다.")
     void findOneItemById() {
-        Item findItem = itemMapper.selectById(1L);
+        Item findItem = testItemMapper.selectById(1L);
 
         assertThat(findItem).isNotNull();
         assertThat(findItem.getId()).isEqualTo(1L);
@@ -33,7 +33,7 @@ class DoubleShopApplicationTests {
     @Order(2)
     @DisplayName("select 목록 테스트: 모든 상품 목록을 검색할 경우, 상품 갯수는 총 14개여야 한다.")
     void findAllItems() {
-        List<Item> allItems = itemMapper.selectAllItems();
+        List<Item> allItems = testItemMapper.selectAllItems();
 
         assertThat(allItems).isNotNull();
         assertThat(allItems.size()).isEqualTo(14);
@@ -43,10 +43,10 @@ class DoubleShopApplicationTests {
     @Order(3)
     @DisplayName("update 테스트: 1번 상품의 이름을 바꿀 경우, 기존 1번 상품의 이름과 달라야 한다.")
     void updateOneItem() {
-        Item item = itemMapper.selectById(1L);
+        Item item = testItemMapper.selectById(1L);
         String beforeChangeName = item.getName();
         item.setName("changed");
-        itemMapper.updateItem(item);
+        testItemMapper.updateItem(item);
         String afterChangeNAme = item.getName();
 
         assertThat(item).isNotNull();
@@ -57,9 +57,9 @@ class DoubleShopApplicationTests {
     @Order(4)
     @DisplayName("delete 테스트: 1번 상품을 삭제할 경우, 기존의 상품 목록의 갯수보다 1개 적어야 한다.")
     void deleteOneItem() {
-        int beforeDeleteItemSize = itemMapper.selectAllItems().size();
-        itemMapper.deleteItem(1L);
-        int afterDeleteItemSize = itemMapper.selectAllItems().size();
+        int beforeDeleteItemSize = testItemMapper.selectAllItems().size();
+        testItemMapper.deleteItem(1L);
+        int afterDeleteItemSize = testItemMapper.selectAllItems().size();
         assertThat(beforeDeleteItemSize - 1).isSameAs(afterDeleteItemSize);
     }
 
@@ -67,7 +67,7 @@ class DoubleShopApplicationTests {
     @Order(5)
     @DisplayName("insert 테스트: 상품을 하나 등록할 경우, 추가된 로우는 하나여야만 한다.")
     void insertOneItem() {
-        int affectedRowNum = itemMapper.insertItem(Item.builder()
+        int affectedRowNum = testItemMapper.insertItem(Item.builder()
                 .name("newItem")
                 .brandName("newBrand")
                 .description("newDescription")
