1202fb1abc29cbc3cfdf87476ec0bdc5476032d2
wanniDev <wannidev0928@gmail.com>
2021-10-04 02:05:13 +0900

    Remove: 사용하지 않는 import 패키지 삭제

--- src/main/java/com/project/doubleshop/web/item/controller/api/ItemRestController.java
+++ src/main/java/com/project/doubleshop/web/item/controller/api/ItemRestController.java
@@ -4,7 +4,6 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
