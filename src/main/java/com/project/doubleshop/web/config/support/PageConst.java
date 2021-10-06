c08bd28423b163711e4ad02d4e779218f5315b85
wanniDev <wannidev0928@gmail.com>
2021-10-06 15:15:07 +0900

    Refactor: 페이징 리졸버 내부 로직 변경
    SimpleOffsetRequest
    - 좀더 직관적인 타입 명으로 페이징 요청 클래스명 변경 (SimpleOffsetPageRequest -> SimplePageRequest)
    - 기본 생성자에 상수를 적용하고, 주석을 달아, 의도를 좀더 명시적으로 표시
    - 멤버 변수 명, 리졸버 내부 로직과 연관성 있게 네이밍 변경(limit -> size)
    SimpleOffsetPageableHandlerMethodResolver
    - 기존 상수는 `PageConst`에 분류
    - 추가된 로직에 맞게 로컬 변수 네이밍 변경
    - 두 번 이상 사용된 메소드는 변수로 따로 담아서 사용
    - 기존의 `toInt` 메소드의 exception handling을 unchecked exception 처리로 변경
    ItemMapper.xml
    - 파라미터 타입인 Pageable의 변경된 변수명에 맞게 쿼리 스크립트 변경

--- src/main/java/com/project/doubleshop/web/config/support/PageConst.java
+++ src/main/java/com/project/doubleshop/web/config/support/PageConst.java
@@ -1,2 +1,13 @@
-package com.project.doubleshop.web.config.support;public class PageConst {
+package com.project.doubleshop.web.config.support;
+
+public class PageConst {
+
+	public static final String DEFAULT_PAGE_PARAMETER = "page";
+
+	public static final String DEFAULT_SIZE_PARAMETER = "size";
+
+	public static final int DEFAULT_PAGE_NUMBER = 0;
+
+	public static final int DEFAULT_MAX_SIZE = 9;
+
 }
--- src/main/java/com/project/doubleshop/web/item/exception/InvalidParameterValueException.java
+++ src/main/java/com/project/doubleshop/web/item/exception/InvalidParameterValueException.java
@@ -1,2 +1,24 @@
-package com.project.doubleshop.web.item.exception;public class InvalidParameterValueException {
+package com.project.doubleshop.web.item.exception;
+
+public class InvalidParameterValueException extends RuntimeException {
+	public InvalidParameterValueException() {
+		super();
+	}
+
+	public InvalidParameterValueException(String message) {
+		super(message);
+	}
+
+	public InvalidParameterValueException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public InvalidParameterValueException(Throwable cause) {
+		super(cause);
+	}
+
+	protected InvalidParameterValueException(String message, Throwable cause, boolean enableSuppression,
+		boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
 }
