bf145161c4fb319c72ffd4308ec8c855cdf7dc1d
wanniDev <wannidev0928@gmail.com>
2021-10-08 22:14:10 +0900

    Feat: 카테고리 삭제정책 로직 추가

---
+++ src/main/java/com/project/doubleshop/domain/category/repository/MyBatisCategoryRepository.java
@@ -0,0 +1,51 @@
+package com.project.doubleshop.domain.category.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.project.doubleshop.domain.category.entity.Category;
+import com.project.doubleshop.domain.category.mapper.CategoryMapper;
+import com.project.doubleshop.domain.common.Manageable;
+import com.project.doubleshop.domain.common.Status;
+import com.project.doubleshop.web.common.StatusRequest;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class MyBatisCategoryRepository implements CategoryRepository {
+
+	private final CategoryMapper categoryMapper;
+
+	@Override
+	public boolean save(Category category) {
+		int affectedRowCnt;
+		if (category.getId() != null) {
+			affectedRowCnt = categoryMapper.updateCategory(category);
+		} else {
+			affectedRowCnt = categoryMapper.insertCategory(category);
+		}
+		return affectedRowCnt != 0;
+	}
+
+	@Override
+	public Category findById(Long id) {
+		return categoryMapper.selectByCategoryId(id);
+	}
+
+	@Override
+	public List<Category> findAll() {
+		return categoryMapper.selectAllCategories();
+	}
+
+	@Override
+	public int updateStatus(StatusRequest requestDTO) {
+		return categoryMapper.updateCategoryStatus(requestDTO);
+	}
+
+	@Override
+	public int deleteData(Status status) {
+		return categoryMapper.deleteCategory(status);
+	}
+}
