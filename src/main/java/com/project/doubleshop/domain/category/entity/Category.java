9613b2fb0e1bb34aed766605290d10cb15856a35
wanniDev <wannidev0928@gmail.com>
2021-10-08 17:14:32 +0900

    Refactor: 카테고리 필드 수정
    fk 방식보다는 그냥 카테고리의 속성과 depth level로 구분하는 방식으로 변경.
    join 쿼리문을 최대한 사용하지 않는 방향으로 변경하고자 함.

--- src/main/java/com/project/doubleshop/domain/category/entity/Category.java
+++ src/main/java/com/project/doubleshop/domain/category/entity/Category.java
@@ -1,10 +1,15 @@
 package com.project.doubleshop.domain.category.entity;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
 public class Category {
 	// 카테고리 pk
 	private Long id;
@@ -12,12 +17,23 @@ public class Category {
 	// 카테고리 이름
 	private String name;
 
-	// 부모카테고리 이름
-	private Long parentId;
+	// 카테고리 타입
+	private CategoryType categoryType;
 
 	// 카테고리 레벨
 	private DepthLevel depthLevel;
 
 	// 환불가능 여부
 	private boolean isRefundable;
+
+	@Override
+	public String toString() {
+		return "Category{" +
+			"id=" + id +
+			", name='" + name + '\'' +
+			", categoryType=" + categoryType +
+			", depthLevel=" + depthLevel +
+			", isRefundable=" + isRefundable +
+			'}';
+	}
 }
