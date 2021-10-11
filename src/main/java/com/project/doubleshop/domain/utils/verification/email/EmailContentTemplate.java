e82fc5db45ce159ca882b4f2da5e332dea73d773
wilco82 <caletabuena33@gmail.com>
2021-10-12 08:34:33 +0900

    Refactor: 리뷰 반영
    - 명시적으로 선언된 setter를 애노테이션으로 선언

--- src/main/java/com/project/doubleshop/domain/utils/verification/email/EmailContentTemplate.java
+++ src/main/java/com/project/doubleshop/domain/utils/verification/email/EmailContentTemplate.java
@@ -1,12 +1,11 @@
 package com.project.doubleshop.domain.utils.verification.email;
 
+import lombok.Setter;
+
+@Setter
 public class EmailContentTemplate {
 
     private String verificationNum;
-
-    public void setVerificationNum(String verificationNum) {
-        this.verificationNum = verificationNum;
-    }
 
     public String parse() {
         return String.format("[Double Shop] 인증 번호는 %s입니다.", verificationNum);
