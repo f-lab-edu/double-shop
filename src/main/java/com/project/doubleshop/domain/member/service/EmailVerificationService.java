4a874797b879e002d6a0dab1fa4530878fa746a5
wilco82 <caletabuena33@gmail.com>
2021-10-12 08:24:28 +0900

    Refactor: 리뷰 반영
    - 이메일 설정 관련 데이터들을 application.yml 파일에 저장하고, 서비스 파일의 변수에 주입하는 방식으로 변경.

--- src/main/java/com/project/doubleshop/domain/member/service/EmailVerificationService.java
+++ src/main/java/com/project/doubleshop/domain/member/service/EmailVerificationService.java
@@ -2,7 +2,8 @@ package com.project.doubleshop.domain.member.service;
 
 import com.project.doubleshop.domain.utils.verification.email.EmailContentTemplate;
 import com.project.doubleshop.web.member.dto.EmailVerificationRequestDto;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,15 +12,19 @@ import javax.servlet.http.HttpSession;
 import java.util.Random;
 
 // 비밀번호 찾기를 요청하는 사용자의 이메일 주소로 인증 이메일 전송
-@AllArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class EmailVerificationService {
 
-    public static final String TITLE = "[Double Shop] 인증번호 안내 메일";
-    public static final String SENDER_ADDRESS = "carpedm082@gmail.com";
+    public static String senderAddress;
     public static final int LIMIT_TIME = 60 * 10;
 
     private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    public void setSenderAddress(String address) {
+        senderAddress = address;
+    }
 
     // 인증 번호를 전송하고, 발송 데이터를 세션에 저장
     public void sendEmail(String email, HttpSession session) {
@@ -30,8 +35,8 @@ public class EmailVerificationService {
         SimpleMailMessage message = new SimpleMailMessage();
 
         message.setTo(email);
-        message.setFrom(SENDER_ADDRESS);
-        message.setSubject(TITLE);
+        message.setFrom(senderAddress);
+        message.setSubject("[Double Shop] 인증번호 안내 메일");
         message.setText(content);
         mailSender.send(message);
 
