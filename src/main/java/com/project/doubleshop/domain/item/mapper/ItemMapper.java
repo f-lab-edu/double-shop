4ce176bb661c4efaf4c3bc16ec5fb31716121728
wanniDev <wannidev0928@gmail.com>
2021-10-11 23:54:22 +0900

    Feat: 카테고리별 상품 조회 기능 구현

--- src/main/java/com/project/doubleshop/domain/item/mapper/ItemMapper.java
+++ src/main/java/com/project/doubleshop/domain/item/mapper/ItemMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.project.doubleshop.domain.common.Status;
+import com.project.doubleshop.domain.common.mapper.param.RequestItemsWithCategory;
 import com.project.doubleshop.domain.item.entity.Item;
 import com.project.doubleshop.web.config.support.Pageable;
 import com.project.doubleshop.web.common.StatusRequest;
@@ -14,6 +15,7 @@ public interface ItemMapper {
     int insertItem(Item item);
     Item selectByItemId(Long id);
     List<Item> selectAllItems(Pageable pageable);
+    List<Item> selectItemsWithCategory(RequestItemsWithCategory request);
     int updateItem(Item item);
     int updateItemStatus(StatusRequest statusRequest);
     int deleteItem(Status status);
