692e376f3862ece7abaa3bd49e5860ceee655bef
wilco82 <caletabuena33@gmail.com>
2021-10-12 08:53:32 +0900

    Feat: 로그인 관련 로직 추가
    - 권한 설정, 계정 정지 여부 로직 추가

--- src/main/java/com/project/doubleshop/domain/member/service/LogInService.java
+++ src/main/java/com/project/doubleshop/domain/member/service/LogInService.java
@@ -1,17 +1,19 @@
 package com.project.doubleshop.domain.member.service;
 
 import com.project.doubleshop.domain.exception.MemberNotFoundException;
+import com.project.doubleshop.domain.exception.NotAuthorizedException;
+import com.project.doubleshop.domain.member.entity.Member;
 import com.project.doubleshop.domain.member.repository.MemberInfoRepository;
 import com.project.doubleshop.domain.utils.SHA256EncryptionUtil;
 import com.project.doubleshop.web.member.dto.LogInRequestDto;
 import com.project.doubleshop.web.member.dto.MemberInfoDto;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class LogInService {
 
     private final HttpSession session;
@@ -24,6 +26,8 @@ public class LogInService {
         checkUserIdAndPassword(requestDto);
 
         String userId = requestDto.getUserId();
+
+        setMemberAuthority(userId);
 
         session.setAttribute("userId", userId);
     }
@@ -53,8 +57,22 @@ public class LogInService {
         // Optional 타입으로 값을 리턴하고, MemberInfoDto 객체로 변환한다.
         // 리턴값이 null이라면 MemberNotFoundException 예외를 발생시킨다.
         return repository.findByUserId(userId)
-                .orElseThrow(() -> new MemberNotFoundException("없는 사용자입니다."))
+                .orElseThrow(() -> new MemberNotFoundException("존재하지 않는 사용자입니다."))
                 .toMemberInfoDto();
+    }
+
+    public void setMemberAuthority(String userId) {
+        Member member = repository.findByUserId(userId)
+                .orElseThrow(() -> new MemberNotFoundException("존재하지 않는 사용자입니다."));
+
+        checkIfBanned(member);
+        session.setAttribute("auth", member.getAuthority());
+    }
+
+    private void checkIfBanned(Member member) {
+        if (member.isBanned()) {
+            throw new NotAuthorizedException("관리자에 의해 정지된 사용자입니다.");
+        }
     }
 
 }
