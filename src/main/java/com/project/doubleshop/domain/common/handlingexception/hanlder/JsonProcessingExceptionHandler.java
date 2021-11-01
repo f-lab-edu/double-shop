20e8999d1e3e4da38f3d39eaae70d39316e5efe1
wanniDev <wannidev0928@gmail.com>
2021-11-01 15:47:52 +0900

    Feat: 400 Status, Json 파싱 과정에서 발생하는 Exception Handler 추가

---
+++ src/main/java/com/project/doubleshop/domain/common/handlingexception/hanlder/JsonProcessingExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.project.doubleshop.domain.common.handlingexception.hanlder;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+@Component
+@Order(Ordered.LOWEST_PRECEDENCE)
+public class JsonProcessingExceptionHandler extends AbstractBadRequestExceptionHandler<JsonProcessingException> {
+
+	public JsonProcessingExceptionHandler() {
+
+		super(JsonProcessingException.class.getSimpleName());
+		log.info("Created");
+	}
+}
