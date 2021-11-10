edebdb1dea74f9df6da53ae2078496366e636382
wilco82 <caletabuena33@gmail.com>
2021-11-10 17:23:59 +0900

    Refactor: static 구문 삭제
    - yml 파일 통합으로 불필요한 설정 코드 삭제

--- src/main/java/com/project/doubleshop/DoubleShopApplication.java
+++ src/main/java/com/project/doubleshop/DoubleShopApplication.java
@@ -6,11 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DoubleShopApplication {
 
-    static {
-        System.setProperty("spring.config.location",
-                "classpath:/application.yml,classpath:/application-keystore.yml");
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(DoubleShopApplication.class, args);
     }
--- src/test/java/com/project/doubleshop/DoubleShopApplicationTests.java
+++ src/test/java/com/project/doubleshop/DoubleShopApplicationTests.java
@@ -6,11 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DoubleShopApplicationTests {
 
-    static {
-        System.setProperty("spring.config.location",
-                "classpath:/application.yml,classpath:/application-keystore.yml");
-    }
-
     @Test
     void contextLoads() {
     }
