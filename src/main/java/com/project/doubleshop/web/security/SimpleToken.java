f8502c5c98948e0d2c4103d6887f0163d01d8173
wanniDev <wannidev0928@gmail.com>
2021-11-13 01:48:21 +0900

    Feat: SimpleToken 구현
     FilterChain을 타면서 요청 헤더의 유효한 토큰 키값을 조회하면,
    얻을 수 있는 토큰 인증값 오브젝트를 받을 수 있다.
    탈취당할 수도 있는 경우에 대비하여, 회원 모델의
    모든 개인 정보 중에 민감하지 않은 부분만 추출한다.

---
+++ src/main/java/com/project/doubleshop/web/security/SimpleToken.java
@@ -0,0 +1,46 @@
+package com.project.doubleshop.web.security;
+
+import java.util.Arrays;
+import java.util.Date;
+
+import lombok.Getter;
+
+@Getter
+public class SimpleToken {
+
+	private Long userId;
+
+	private String name;
+
+	private String email;
+
+	private Date issuedAt;
+
+	private Date expiredAt;
+
+	private String[] roles;
+
+	public SimpleToken(Long userId, String name, String email, Date issuedAt, Date expiredAt, String[] roles) {
+		this.userId = userId;
+		this.name = name;
+		this.email = email;
+		this.issuedAt = issuedAt;
+		this.expiredAt = expiredAt;
+		this.roles = roles;
+	}
+
+	public void resetExpiry(int expirySeconds) {
+		Date now = new Date();
+		this.expiredAt = new Date(now.getTime() + expirySeconds * 1000L);
+	}
+
+	@Override
+	public String toString() {
+		return "SimpleToken{" +
+			"userId=" + userId +
+			", issuedAt=" + issuedAt +
+			", expiredAt=" + expiredAt +
+			", roles=" + Arrays.toString(roles) +
+			'}';
+	}
+}
