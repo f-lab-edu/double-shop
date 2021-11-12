a0fbfa905e6a4fd448c5500e679bc4e0f2f2ce21
wanniDev <wannidev0928@gmail.com>
2021-11-13 03:18:34 +0900

    Feat: SimpleAuthenticationProvider 구현
    AuthenticationManager에 의해 위임 받는 ProviderManager의 구현체

---
+++ src/main/java/com/project/doubleshop/web/security/SimpleAuthenticationProvider.java
@@ -0,0 +1,62 @@
+package com.project.doubleshop.web.security;
+
+import static com.project.doubleshop.web.security.SimpleAuthenticationTokenFilter.*;
+import static org.springframework.security.core.authority.AuthorityUtils.*;
+import static org.springframework.util.TypeUtils.*;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.project.doubleshop.domain.exception.MemberNotFoundException;
+import com.project.doubleshop.domain.member.entity.Member;
+import com.project.doubleshop.domain.member.service.MemberService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class SimpleAuthenticationProvider implements AuthenticationProvider {
+
+	private final MemberService memberService;
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		SimpleAuthenticationToken authenticationToken = (SimpleAuthenticationToken) authentication;
+		return processMemberAuthentication(authenticationToken.authenticationRequest());
+	}
+
+	private Authentication processMemberAuthentication(AuthenticationRequest request) {
+		try {
+			Member member = memberService.login(request.getPrincipal(), request.getCredential());
+			SimpleAuthenticationToken authenticated =
+				new SimpleAuthenticationToken(member.getId(), null, createAuthorityList(Role.USER.value()));
+
+			// 세션 서버 선택이 확정되면, 코드 변경.
+			String tokenKey = UUID.randomUUID().toString();
+			SimpleToken tokenValue = new SimpleToken(member.getUserId(), member.getName(), member.getEmail(),
+				new String[] {Role.USER.value()});
+
+			sessionMap.put(tokenKey, tokenValue);
+			authenticated.setDetails(new AuthenticationResult(tokenKey, tokenValue));
+			return authenticated;
+		} catch (MemberNotFoundException e) {
+			throw new UsernameNotFoundException(e.getMessage());
+		} catch (IllegalArgumentException e) {
+			throw new BadCredentialsException(e.getMessage());
+		} catch (DataAccessException e) {
+			throw new AuthenticationServiceException(e.getMessage(), e);
+		}
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return isAssignable(SimpleAuthenticationToken.class, authentication);
+	}
+}
