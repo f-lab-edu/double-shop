64dc9324d77f5bc433371bacac3276c533300d64
wanniDev <wannidev0928@gmail.com>
2021-11-15 01:43:10 +0900

    Feat: 인증 repository 인터페이스 추가

---
+++ src/main/java/com/project/doubleshop/domain/member/repository/AuthMemberRepository.java
@@ -0,0 +1,11 @@
+package com.project.doubleshop.domain.member.repository;
+
+import com.project.doubleshop.domain.member.entity.v2.Member;
+
+public interface AuthMemberRepository {
+	boolean save(Member entity);
+
+	Member findById(Long id);
+
+	Member findByEmail(String email);
+}
