31fb22dfbd74bbb5421a8f7ce5fc2e766ebc8dc4
wanniDev <wannidev0928@gmail.com>
2021-11-28 15:53:43 +0900

    Refactor: properties 파일 변형 및 통합

--- src/main/java/com/project/doubleshop/web/config/security/redis/RedisConfig.java
+++ src/main/java/com/project/doubleshop/web/config/security/redis/RedisConfig.java
@@ -10,7 +10,6 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@PropertySource("classpath:/application-redis.yml")
 public class RedisConfig {
 
 	@Bean
