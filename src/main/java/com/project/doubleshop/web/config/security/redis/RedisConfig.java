3fb24183b7f9b1992e7667382ac4d65d93a2c950
wanniDev <wannidev0928@gmail.com>
2021-11-30 13:54:04 +0900

    Feat: 기능별로 connection을 mutable하게 적용시킬 수 있는 설정 적용

--- src/main/java/com/project/doubleshop/web/config/security/redis/RedisConfig.java
+++ src/main/java/com/project/doubleshop/web/config/security/redis/RedisConfig.java
@@ -1,9 +1,10 @@
 package com.project.doubleshop.web.config.security.redis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -12,19 +13,26 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+	@Value("${spring.redis.host}")
+	private String redisHost;
+
+	@Value("${spring.redis.token.port}")
+	private int redisTokenPort;
+
 	@Bean
-	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory();
+	public RedisConnectionFactory redisTokenConnectionFactory() {
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+		redisStandaloneConfiguration.setHostName(redisHost);
+		redisStandaloneConfiguration.setPort(redisTokenPort);
+		return new LettuceConnectionFactory(redisStandaloneConfiguration);
 	}
 
 	@Bean
-	public RedisTemplate<String, Object> redisTemplate() {
-		GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer();
+	public RedisTemplate<String, Object> tokenSessionManagementRedisTemplate() {
 		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		redisTemplate.setConnectionFactory(redisTokenConnectionFactory());
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
-		redisTemplate.setValueSerializer(serializer);
-
+		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 		return redisTemplate;
 	}
 }
