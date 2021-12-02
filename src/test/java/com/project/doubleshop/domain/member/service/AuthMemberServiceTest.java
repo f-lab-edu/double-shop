d772e7be0e1ad31e6df94538bcbae05fa930b42e
wanniDev <wannidev0928@gmail.com>
2021-12-02 22:15:49 +0900

    Test: 회원 도메인 테스트 코드 추가

---
+++ src/test/java/com/project/doubleshop/domain/member/service/AuthMemberServiceTest.java
@@ -0,0 +1,87 @@
+package com.project.doubleshop.domain.member.service;
+
+import static com.project.doubleshop.domain.member.service.MockMember.Member1.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.project.doubleshop.domain.exception.MemberNotFoundException;
+import com.project.doubleshop.domain.member.repository.AuthMemberRepository;
+import com.project.doubleshop.web.member.dto.JoinRequest;
+
+@ExtendWith(MockitoExtension.class)
+class AuthMemberServiceTest {
+
+	@Mock
+	AuthMemberRepository authMemberRepository;
+
+	@Mock
+	PasswordEncoder passwordEncoder;
+
+	@InjectMocks
+	AuthMemberService authMemberService;
+
+	@Test
+	@DisplayName("로그인 로직 동작 테스트")
+	void loginTest() {
+		given(authMemberRepository.findByUserId(any())).willReturn(MEMBER);
+		given(passwordEncoder.matches(PASSWORD, PASSWORD)).willReturn(true);
+
+		authMemberService.login(USER_ID, PASSWORD);
+
+		then(authMemberRepository).should(times(1)).save(MEMBER);
+	}
+
+	@Test
+	@DisplayName("로그인 실패 - 없는 계정 입력")
+	void loginFailTest() {
+		given(authMemberRepository.findByUserId(any())).willReturn(null);
+
+		assertThrows(MemberNotFoundException.class, () -> authMemberService.login(USER_ID, PASSWORD));
+	}
+
+	@Test
+	@DisplayName("로그인 실패 - 잘못된 비밀번호 입력")
+	void loginWithWrongPassword() {
+		given(authMemberRepository.findByUserId(any())).willReturn(MEMBER);
+		given(passwordEncoder.matches(PASSWORD, PASSWORD)).willReturn(false);
+
+		assertThrows(IllegalArgumentException.class, () -> authMemberService.login(USER_ID, PASSWORD));
+	}
+
+	@Test
+	@DisplayName("이메일로 사용자 검색 실패")
+	void notFoundEmail() {
+		given(authMemberRepository.findByEmail(any())).willReturn(null);
+
+		assertThat(authMemberService.findByEmail(EMAIL)).isEmpty();
+	}
+
+	@Test
+	@DisplayName("회원가입 로직 동작 테스트")
+	void joinTest() {
+		authMemberService.join(USER_ID, PASSWORD, NAME, EMAIL, PHONE);
+
+		then(authMemberRepository).should(times(1)).save(any());
+	}
+
+	@Test
+	@DisplayName("이메일 중복 동작 테스트")
+	void emailTest() {
+		authMemberService.findByEmail(EMAIL);
+
+		then(authMemberRepository).should(times(1)).findByEmail(EMAIL);
+	}
+}
