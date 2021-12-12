ade864c4ca69272b63daafa6a59c736393eeb1bd
wilco82 <caletabuena33@gmail.com>
2021-12-12 22:38:26 +0900

    Feat: 주문 DTO 클래스 추가
    - 상품 ID, 주문 수량 포함

---
+++ src/main/java/com/project/doubleshop/web/order/dto/OrderDto.java
@@ -0,0 +1,14 @@
+package com.project.doubleshop.web.order.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderDto {
+
+    private Long itemId;
+
+    private int count;
+
+}
