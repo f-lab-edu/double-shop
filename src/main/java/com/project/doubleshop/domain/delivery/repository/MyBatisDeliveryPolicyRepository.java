b3138532010b9d0d8658a69efe0b1e20abe8a90e
wanniDev <wannidev0928@gmail.com>
2021-12-13 01:41:20 +0900

    Feat: 배송 정책, 배송 기사 데이터 액세스 로직 구현

---
+++ src/main/java/com/project/doubleshop/domain/delivery/repository/MyBatisDeliveryPolicyRepository.java
@@ -0,0 +1,39 @@
+package com.project.doubleshop.domain.delivery.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.project.doubleshop.domain.delivery.entity.DeliveryPolicy;
+import com.project.doubleshop.domain.delivery.mapper.DeliveryPolicyMapper;
+import com.project.doubleshop.web.config.support.Pageable;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class MyBatisDeliveryPolicyRepository implements DeliveryPolicyRepository {
+
+	private final DeliveryPolicyMapper mapper;
+
+	@Override
+	public boolean save(DeliveryPolicy deliveryPolicy) {
+		int affectedRowCnt;
+		if (deliveryPolicy.getId() != null) {
+			affectedRowCnt = mapper.updateDeliveryPolicy(deliveryPolicy);
+		} else {
+			affectedRowCnt = mapper.insertDeliveryPolicy(deliveryPolicy);
+		}
+		return affectedRowCnt != 0;
+	}
+
+	@Override
+	public DeliveryPolicy findById(Long id) {
+		return mapper.selectByDeliveryPolicy(id);
+	}
+
+	@Override
+	public List<DeliveryPolicy> findAll(Pageable pageable) {
+		return mapper.selectAllDeliveryPolicy(pageable);
+	}
+}
