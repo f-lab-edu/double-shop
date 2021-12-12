46d268cc082b778f63c64235deb83395ae61ee25
wilco82 <caletabuena33@gmail.com>
2021-12-12 23:16:32 +0900

    Feat: 주문 내역 DTO 생성
    - 주문 객체를 파라미터로 받는 생성자 정의. 주문 날짜는 특정 문자열 패턴으로 전달하기 위해 포맷 수정.
    - DTO 객체를 주문 상품 리스트에 추가하는 메서드 정의

---
+++ src/main/java/com/project/doubleshop/web/order/dto/OrderHistoryDto.java
@@ -0,0 +1,34 @@
+package com.project.doubleshop.web.order.dto;
+
+import com.project.doubleshop.domain.order.entity.Order;
+import com.project.doubleshop.domain.order.entity.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class OrderHistoryDto {
+
+    private Long orderId;
+
+    private String orderDate;
+
+    private OrderStatus orderStatus;
+
+    private List<OrderItemDto> orderItemDtoList = new ArrayList<>(); // 주문 상품 리스트
+
+    public OrderHistoryDto(Order order) {
+        this.orderId = order.getId();
+        this.orderDate = order.getOrderDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        this.orderStatus = order.getStatus();
+    }
+
+    public void addOrderItemDto(OrderItemDto orderItemDto) {
+        orderItemDtoList.add(orderItemDto); // DTO 객체를 주문 상품 리스트에 추가
+    }
+
+}
