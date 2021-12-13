9d5671d083f484ff09443b03fd535e31d4739e74
wilco82 <caletabuena33@gmail.com>
2021-12-13 13:39:48 +0900

    Feat: 주문 Repository 메서드 추가 및 삭제
    - 회원 주문 데이터 조회 메서드 정의
    - 회원의 주문 개수 조회 메서드 정의

--- src/main/java/com/project/doubleshop/domain/order/repository/OrderRepository.java
+++ src/main/java/com/project/doubleshop/domain/order/repository/OrderRepository.java
@@ -1,21 +1,18 @@
 package com.project.doubleshop.domain.order.repository;
 
 import com.project.doubleshop.domain.order.entity.Order;
+import com.project.doubleshop.web.config.support.Pageable;
 
 import java.util.List;
 
 public interface OrderRepository {
 
-    /* Order를 저장하는 메서드 */
     public void save(Order order);
 
-    /* 단건 조회 메서드 */
     public Order findById(Long id);
 
-    public List<Order> findAllByString(OrderSearch orderSearch);
+    public List<Order> findAll(String userId, Pageable pageable);
 
-    public List<Order> findAllWithMemberDelivery();
-
-    public List<Order> findAllWithItem();
+    public Long count(String userId);
 
 }
