b8d387490e879c4a723de241fff1452d3786d374
wilco82 <caletabuena33@gmail.com>
2021-12-14 00:16:44 +0900

    Feat: 주문 컨트롤러에 조회 메서드 추가
    - 주문 컨트롤러에 구매 내역을 조회하는 로직을 호출하는 메서드 생성

--- src/main/java/com/project/doubleshop/web/order/controller/OrderController.java
+++ src/main/java/com/project/doubleshop/web/order/controller/OrderController.java
@@ -1,13 +1,19 @@
 package com.project.doubleshop.web.order.controller;
 
+import com.project.doubleshop.domain.order.entity.Order;
 import com.project.doubleshop.domain.order.service.OrderService;
+import com.project.doubleshop.web.config.support.Pageable;
+import com.project.doubleshop.web.config.support.SimplePageRequest;
 import com.project.doubleshop.web.order.dto.OrderDto;
+import com.project.doubleshop.web.order.dto.OrderHistoryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api")
@@ -16,7 +22,7 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @PostMapping(value = "order")
+    @PostMapping("order")
     public ResponseEntity order(@RequestBody OrderDto orderDto, Principal principal) {
         String userId = principal.getName(); // 로그인한 회원의 ID 정보 조회
 
@@ -29,6 +35,16 @@ public class OrderController {
         }
 
         return new ResponseEntity<Long>(orderId, HttpStatus.OK);
+    }
+
+    @GetMapping({"orders", "orders/{page}"})
+    public ResponseEntity orderHistory(@PathVariable("page") Optional<Integer> page,
+                               Principal principal) {
+        Pageable pageable = new SimplePageRequest(page.orElse(0), 4);
+
+        List<OrderHistoryDto> orderHistoryDtoList = orderService.findOrders(principal.getName(), pageable);
+
+        return new ResponseEntity<List<OrderHistoryDto>>(orderHistoryDtoList, HttpStatus.OK);
     }
 
 }
