68dc3dbcdf16350d0056a64e1bd92378e7330c93
wilco82 <caletabuena33@gmail.com>
2021-12-28 13:45:10 +0900

    Refactor: 주문 엔티티 변수명 수정

--- src/main/java/com/project/doubleshop/domain/order/entity/Order.java
+++ src/main/java/com/project/doubleshop/domain/order/entity/Order.java
@@ -1,9 +1,9 @@
 package com.project.doubleshop.domain.order.entity;
 
+import com.project.doubleshop.domain.common.Status;
 import com.project.doubleshop.domain.member.entity.Member;
 import lombok.*;
 
-import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,17 +17,16 @@ public class Order {
 
     private Member member; // 회원 정보
 
-    private Long couponId; // 쿠폰
-
     private List<OrderItem> orderItems = new ArrayList<>(); // 주문 상품
 
     private LocalDateTime orderDate; // 주문일자
 
-    private OrderStatus status; // 주문 상태
+    private OrderStatus orderStatus; // 주문 상태
 
     private Long totalPrice; // 총 상품 가격
 
-    @PastOrPresent(message = "field 'statusUpdateTime' must be present or past")
+    private Status status; // 상태
+
     private LocalDateTime statusUpdateTime; // 상태 업데이트 시간
 
     public void addOrderItem(OrderItem orderItem) {
@@ -44,7 +43,7 @@ public class Order {
             order.addOrderItem(orderItem); // 여러 개의 상품을 주문할 수 있도록 Order 객체에 orderItem 추가
         }
 
-        order.setStatus(OrderStatus.ORDERED);
+        order.setOrderStatus(OrderStatus.ORDERED);
         order.setOrderDate(LocalDateTime.now());
 
         return order;
@@ -59,7 +58,7 @@ public class Order {
     }
 
     public void cancelOrder() {
-        status = OrderStatus.CANCELED;
+        orderStatus = OrderStatus.CANCELED;
 
         for (OrderItem orderItem : orderItems) {
             orderItem.cancel();
