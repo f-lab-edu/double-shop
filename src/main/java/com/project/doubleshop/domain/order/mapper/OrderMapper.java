a1fbb3cbed2831ebf2b3c99ae4e0615fc17514c5
wilco82 <caletabuena33@gmail.com>
2021-12-28 13:49:19 +0900

    Feat: 주문 Mapper 작성

--- src/main/java/com/project/doubleshop/domain/order/mapper/OrderMapper.java
+++ src/main/java/com/project/doubleshop/domain/order/mapper/OrderMapper.java
@@ -1,6 +1,9 @@
 package com.project.doubleshop.domain.order.mapper;
 
+import com.project.doubleshop.domain.common.Status;
 import com.project.doubleshop.domain.order.entity.Order;
+import com.project.doubleshop.web.common.StatusRequest;
+import com.project.doubleshop.web.config.support.Pageable;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -9,6 +12,8 @@ import java.util.List;
 public interface OrderMapper {
     int insertOrder(Order order);
     Order selectByOrderId(Long id);
-    List<Order> selectAll();
+    List<Order> selectAllOrders(Pageable pageable);
     int updateOrder(Order order);
+    int updateOrderStatus(StatusRequest statusRequest);
+    int deleteOrder(Status status);
 }
