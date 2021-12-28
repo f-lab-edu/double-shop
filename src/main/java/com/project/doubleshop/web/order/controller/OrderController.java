b353cc4623f725e25a9a6b3c2f41a970a1583470
wilco82 <caletabuena33@gmail.com>
2021-12-28 13:55:46 +0900

    Refactor: 인증 과정에서 SimpleAuthentication 사용

--- src/main/java/com/project/doubleshop/web/order/controller/OrderController.java
+++ src/main/java/com/project/doubleshop/web/order/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.project.doubleshop.web.order.controller;
 
 import com.project.doubleshop.domain.order.service.OrderService;
+import com.project.doubleshop.web.config.security.SimpleAuthentication;
 import com.project.doubleshop.web.config.support.Pageable;
 import com.project.doubleshop.web.config.support.SimplePageRequest;
 import com.project.doubleshop.web.order.dto.OrderDto;
@@ -8,8 +9,7 @@ import com.project.doubleshop.web.order.dto.OrderHistoryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,9 +23,9 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("order")
-    public ResponseEntity order(@RequestBody OrderDto orderDto, Authentication authentication) {
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        String userId = userDetails.getUsername(); // 로그인한 회원의 ID 정보 조회
+    public ResponseEntity order(@RequestBody OrderDto orderDto,
+                                @AuthenticationPrincipal SimpleAuthentication authentication) {
+        String userId = authentication.getUserId(); // 로그인한 회원의 ID 정보 조회
 
         Long orderId;
 
@@ -40,21 +40,22 @@ public class OrderController {
 
     @GetMapping({"orders", "orders/{page}"})
     public ResponseEntity orderHistory(@PathVariable("page") Optional<Integer> page,
-                               Authentication authentication) {
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+                                       @AuthenticationPrincipal SimpleAuthentication authentication) {
+        String userId = authentication.getUserId();
 
         Pageable pageable = new SimplePageRequest(page.orElse(0), 4);
 
-        List<OrderHistoryDto> orderHistoryDtoList = orderService.findOrders(userDetails.getUsername(), pageable);
+        List<OrderHistoryDto> orderHistoryDtoList = orderService.findOrders(userId, pageable);
 
         return new ResponseEntity<List<OrderHistoryDto>>(orderHistoryDtoList, HttpStatus.OK);
     }
 
     @PostMapping("order/{orderId}/cancel")
-    public ResponseEntity cancelOrder(@PathVariable("orderId") Long orderId, Authentication authentication) {
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+    public ResponseEntity cancelOrder(@PathVariable("orderId") Long orderId,
+                                      @AuthenticationPrincipal SimpleAuthentication authentication) {
+        String userId = authentication.getUserId();
 
-        if (!orderService.validateOrder(orderId, userDetails.getUsername())) {
+        if (!orderService.validateOrder(orderId, userId)) {
             return new ResponseEntity<String>("권한이 없습니다.", HttpStatus.FORBIDDEN);
         }
 
