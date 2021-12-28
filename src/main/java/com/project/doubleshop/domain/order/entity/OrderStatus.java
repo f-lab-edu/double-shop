85d521c4bc9cb74d7a2cdc06f2860b2fb9b86b38
wilco82 <caletabuena33@gmail.com>
2021-12-28 13:51:37 +0900

    Feat: 주문 상태 enum 파일 작성

--- src/main/java/com/project/doubleshop/domain/order/entity/OrderStatus.java
+++ src/main/java/com/project/doubleshop/domain/order/entity/OrderStatus.java
@@ -1,6 +1,30 @@
 package com.project.doubleshop.domain.order.entity;
 
+import static com.project.doubleshop.domain.order.entity.OrderStatusConstants.CANCELED_CODE;
+import static com.project.doubleshop.domain.order.entity.OrderStatusConstants.ORDERED_CODE;
+
 public enum OrderStatus {
-    ORDERED,
-    CANCELED
+    ORDERED(ORDERED_CODE),
+    CANCELED(CANCELED_CODE);
+
+    private final int i;
+
+    OrderStatus(int i) {
+        this.i = i;
+    }
+
+    public static OrderStatus of(int value) {
+        switch (value) {
+            case ORDERED_CODE:
+                return OrderStatus.ORDERED;
+            case CANCELED_CODE:
+                return OrderStatus.CANCELED;
+            default:
+                throw new AssertionError(String.format("Unknown OrderStatus value : %d", value));
+        }
+    }
+
+    public int getValue() {
+        return i;
+    }
 }
---
+++ src/main/java/com/project/doubleshop/domain/order/entity/OrderStatusConstants.java
@@ -0,0 +1,6 @@
+package com.project.doubleshop.domain.order.entity;
+
+public class OrderStatusConstants {
+    public static final int ORDERED_CODE = 5001;
+    public static final int CANCELED_CODE = 5002;
+}
