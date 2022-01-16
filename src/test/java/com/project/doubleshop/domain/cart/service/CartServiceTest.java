3ae91f264eee1c4f24cf8461258b6ee6774a91c4
wanniDev <wannidev0928@gmail.com>
2022-01-17 00:34:35 +0900

    Test: 변형된 메소드에 따라, 테스트 수정

--- src/test/java/com/project/doubleshop/domain/cart/service/CartServiceTest.java
+++ src/test/java/com/project/doubleshop/domain/cart/service/CartServiceTest.java
@@ -1,18 +1,22 @@
 package com.project.doubleshop.domain.cart.service;
 
 import static com.project.doubleshop.domain.cart.service.MockCart.Cart1.*;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.project.doubleshop.domain.cart.entity.Cart;
 import com.project.doubleshop.domain.cart.repository.CartRepository;
 import com.project.doubleshop.domain.member.service.MockMember;
 
@@ -49,10 +53,11 @@ class CartServiceTest {
 	@DisplayName("장바구니 삭제 테스트")
 	void deleteCarts() {
 		List<Long> cardIds = List.of(ID);
-		given(cartRepository.deleteCarts(anyList())).willReturn(cardIds.size());
+		given(cartRepository.deleteCarts(anyLong(), anyList())).willReturn(cardIds.size());
+		given(cartRepository.findCartInIds(cardIds, MockMember.Member1.ID)).willReturn(List.of(new Cart(1L, 1L, 1L)));
 
-		cartService.deleteCarts(cardIds);
+		Integer resultSize = cartService.deleteCarts(1L, cardIds);
 
-		then(cartRepository).should(times(1)).deleteCarts(cardIds);
+		assertThat(resultSize).isEqualTo(cardIds.size());
 	}
 }
