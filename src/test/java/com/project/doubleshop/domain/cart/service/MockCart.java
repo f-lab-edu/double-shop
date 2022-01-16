48a4543845297fb31b8d44f6db966fe5bf1127bf
wanniDev <wannidev0928@gmail.com>
2022-01-16 14:14:02 +0900

    Test: 장바구니 서비스 테스트

---
+++ src/test/java/com/project/doubleshop/domain/cart/service/MockCart.java
@@ -0,0 +1,17 @@
+package com.project.doubleshop.domain.cart.service;
+
+import com.project.doubleshop.domain.cart.entity.Cart;
+
+public class MockCart {
+	public static class Cart1 {
+		public static final Long ID = 1L;
+		public static final Long MEMBER_ID = 1L;
+		public static final Long ITEM_ID = 1L;
+
+		public static final Cart CART = Cart.builder()
+			.id(ID)
+			.memberId(MEMBER_ID)
+			.itemId(ITEM_ID)
+			.build();
+	}
+}
