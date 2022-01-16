a3441cfd66b39011abdf9e5b017fa628759c40eb
wanniDev <wannidev0928@gmail.com>
2022-01-16 19:28:59 +0900

    Feat: 장바구니 등록과 삭제 api 추가

--- src/main/java/com/project/doubleshop/web/cart/controller/CartRestController.java
+++ src/main/java/com/project/doubleshop/web/cart/controller/CartRestController.java
@@ -1,12 +1,15 @@
 package com.project.doubleshop.web.cart.controller;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +30,21 @@ public class CartRestController {
 	public ResponseEntity<List<CartDto>> findCarts(@PathVariable Long id) {
 		List<Cart> carts = cartService.findCartsByMemberId(id);
 		return ResponseEntity.ok(carts.stream().map(CartDto::new).collect(Collectors.toList()));
+	}
+
+	@PostMapping("member/{memberId}/cart/{itemId}")
+	public ResponseEntity<CartDto> saveNewCart(@PathVariable Long memberId,
+		@PathVariable Long itemId, @RequestBody CartDto cartDto) {
+		Cart cart = Cart.builder()
+			.memberId(memberId)
+			.itemId(itemId)
+			.build();
+		return ResponseEntity.ok(new CartDto(cartService.saveNewCart(cart)));
+	}
+
+	@DeleteMapping("member/{id}/cart")
+	public ResponseEntity<Integer> deleteCart(@PathVariable Long id,
+		@RequestBody Map<String, List<Long>> requestMap) {
+		return ResponseEntity.ok(cartService.deleteCarts(id, requestMap.get("itemIds")));
 	}
 }
