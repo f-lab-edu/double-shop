4a46f90aba1aa8c7a4384b1e5caf1f6e05f49259
wanniDev <wannidev0928@gmail.com>
2022-01-21 00:32:04 +0900

    Feat: 주문 서비스 관련 트랜잭션에 필요한 repository 구현

---
+++ src/main/java/com/project/doubleshop/domain/order/repository/OrderDetailRepository.java
@@ -0,0 +1,20 @@
+package com.project.doubleshop.domain.order.repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.project.doubleshop.domain.common.Status;
+import com.project.doubleshop.domain.order.entity.OrderDetail;
+import com.project.doubleshop.web.order.dto.OrderDetailResult;
+
+public interface OrderDetailRepository {
+	boolean save(OrderDetail orderDetail);
+
+	List<OrderDetail> findById(Long orderId);
+
+	List<OrderDetailResult> findWithItemByOrderId(Long orderId);
+
+	Integer updateStatus(Integer statusCode, LocalDateTime statusUpdateTime, List<Long> orderIds);
+
+	Integer deleteOrderDetails(Status status);
+}
--- src/main/java/com/project/doubleshop/domain/order/repository/OrderRepository.java
+++ src/main/java/com/project/doubleshop/domain/order/repository/OrderRepository.java
@@ -1,18 +1,23 @@
 package com.project.doubleshop.domain.order.repository;
 
+import com.project.doubleshop.domain.common.Status;
 import com.project.doubleshop.domain.order.entity.Order;
-import org.springframework.data.domain.Pageable;
+import com.project.doubleshop.web.config.support.Pageable;
+import com.project.doubleshop.web.order.dto.OrderStatusRequest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface OrderRepository {
-
     boolean save(Order order);
 
     Order findById(Long id);
 
-    List<Order> findAll(String userId, Pageable pageable);
+    List<Order> findByMemberId(Long memberId, Pageable pageable);
 
-    Long count(String userId);
+    Integer updateOrderStatus(OrderStatusRequest orderStatusRequest);
 
+    Integer updateStatus(Integer statusCode, LocalDateTime statusUpdateTime, List<Long> orderIds);
+
+    Integer deleteOrders(Status status);
 }
