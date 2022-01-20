ffef75e9d87d663d9a09dab8d4e5fd5a53d45593
wanniDev <wannidev0928@gmail.com>
2022-01-20 23:46:23 +0900

    Feat: 주문 서비스 트랜잭션에 필요한 SQL 쿼리 매핑

---
+++ src/main/java/com/project/doubleshop/domain/order/mapper/OrderDetailMapper.java
@@ -0,0 +1,18 @@
+package com.project.doubleshop.domain.order.mapper;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.project.doubleshop.domain.order.entity.OrderDetail;
+import com.project.doubleshop.web.order.dto.OrderDetailResult;
+
+@Mapper
+public interface OrderDetailMapper {
+	int insertOrderDetail(OrderDetail orderDetail);
+	List<OrderDetail> selectByOrderId(Long orderId);
+	List<OrderDetailResult> selectWithItemByOrderId(Long orderId);
+	int updateStatus(int statusCode, LocalDateTime statusUpdateTime, List<Long> orderIds);
+	int deleteOrderDetails(Integer statusCode);
+}
