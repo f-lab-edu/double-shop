ac8465956d4999f6379da5e178747bdb8fb853f3
wanniDev <wannidev0928@gmail.com>
2022-02-07 18:55:14 +0900

    Refactor: aop를 활용한 예외 핸들링 코드에 제네릭을 활용

---
+++ src/main/java/com/project/doubleshop/web/common/exhandler/dto/ErrorResponse.java
@@ -0,0 +1,43 @@
+package com.project.doubleshop.web.common.exhandler.dto;
+
+import java.net.URI;
+import java.time.LocalDateTime;
+
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.project.doubleshop.domain.exception.ServiceException;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+public class ErrorResponse {
+	private LocalDateTime timeStamp;
+	private Integer status;
+	private String message;
+	private String path;
+
+	public static <T extends ServiceException> ErrorResponse configure(T exception) {
+		URI location = ServletUriComponentsBuilder
+			.fromCurrentRequest()
+			.build()
+			.toUri();
+
+		return ErrorResponse.builder()
+			.timeStamp(LocalDateTime.now())
+			.status(exception.getStatus())
+			.message(exception.getMessage())
+			.path(location.getPath())
+			.build();
+	}
+
+	public boolean inComplete() {
+		return timeStamp == null || status == null || message == null || path == null;
+	}
+}
