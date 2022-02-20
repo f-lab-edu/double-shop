7dd54b62181fd78f92d9321af819fc5fb6d25d03
wanniDev <wannidev0928@gmail.com>
2022-02-20 15:07:25 +0900

    Feat: client-key 숨기기

---
+++ src/main/java/com/project/doubleshop/domain/common/config/AppConfig.java
@@ -0,0 +1,29 @@
+package com.project.doubleshop.domain.common.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+	@Value("${mysql-datasource}")
+	private String dataSourceUrl;
+
+	@Value("${mysql-username}")
+	private String dataSourceUsername;
+
+	@Value("${mysql-password}")
+	private String dataSourcePassword;
+
+	@Bean
+	public DataSource dataSource() {
+		return DataSourceBuilder.create()
+			.url(dataSourceUrl)
+			.username(dataSourceUsername)
+			.password(dataSourcePassword)
+			.build();
+	}
+}
