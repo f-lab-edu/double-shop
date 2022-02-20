9568e7a7ea91abab55dcfe0afd05d314c2d5f57d
wanniDev <wannidev0928@gmail.com>
2022-02-20 16:03:30 +0900

    Feat: propertie 설정 분리

--- src/main/java/com/project/doubleshop/domain/common/config/AppConfig.java
+++ src/main/java/com/project/doubleshop/domain/common/config/AppConfig.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("prod")
 public class AppConfig {
 	@Value("${mysql-datasource}")
 	private String dataSourceUrl;
