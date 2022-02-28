77dcd472bc03e501b565934f15cc57c5e94f30a6
wanniDev <wannidev0928@gmail.com>
2022-02-28 23:03:14 +0900

    Feat: Item, Category 엔티티 매핑

--- src/main/java/com/project/doubleshop/domain/category/entity/Category.java
+++ src/main/java/com/project/doubleshop/domain/category/entity/Category.java
@@ -2,6 +2,13 @@ package com.project.doubleshop.domain.category.entity;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import com.project.doubleshop.domain.common.Status;
 import com.project.doubleshop.web.category.controller.dto.CategoryForm;
 
@@ -11,21 +18,25 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Builder
 @Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class Category {
 	// 카테고리 pk
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	// 카테고리 이름
 	private String name;
 
 	// 카테고리 타입
+	@Enumerated(EnumType.STRING)
 	private CategoryType categoryType;
 
 	// 카테고리 레벨
+	@Enumerated(EnumType.STRING)
 	private DepthLevel depthLevel;
 
 	// 환불가능 여부
