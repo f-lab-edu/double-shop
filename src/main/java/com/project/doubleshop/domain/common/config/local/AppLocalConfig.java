0dd00037c99ed21a20d8077940c3c79bb38b1ef2
wanniDev <wannidev0928@gmail.com>
2022-03-02 02:59:55 +0900

    Feat: 페이징 및 objectmapper 설정 추가

--- src/main/java/com/project/doubleshop/domain/common/config/local/AppLocalConfig.java
+++ src/main/java/com/project/doubleshop/domain/common/config/local/AppLocalConfig.java
@@ -1,5 +1,8 @@
 package com.project.doubleshop.domain.common.config.local;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +13,15 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 
 @Configuration
 @Profile("local")
@@ -36,5 +48,25 @@ public class RedisConfig {
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
 		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 		return redisTemplate;
+	}
+
+	@Bean
+	public Jackson2ObjectMapperBuilder configureObjectMapper() {
+		// Java time module
+		JavaTimeModule jtm = new JavaTimeModule();
+		jtm.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder() {
+			@Override
+			public void configure(ObjectMapper objectMapper) {
+				super.configure(objectMapper);
+				objectMapper.setVisibility(PropertyAccessor.GETTER, JsonAutoDetect.Visibility.NONE);
+				objectMapper.setVisibility(PropertyAccessor.IS_GETTER, JsonAutoDetect.Visibility.NONE);
+				objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+			}
+		};
+		builder.serializationInclusion(JsonInclude.Include.NON_NULL);
+		builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		builder.modulesToInstall(jtm);
+		return builder;
 	}
 }
