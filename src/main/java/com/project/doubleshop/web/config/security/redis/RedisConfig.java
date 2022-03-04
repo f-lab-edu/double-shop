79887620fa7f1f42b26876ad60ed884a4eb39452
wanniDev <wannidev0928@gmail.com>
2022-03-04 23:28:13 +0900

    Feat: profile 설정 분리
    - 설정 환경에 따라, 애플리케이션 코드로 설정 파일을 추가해야했던 구조 개선

--- src/main/java/com/project/doubleshop/web/config/security/redis/RedisConfig.java
+++ src/main/java/com/project/doubleshop/web/config/security/redis/RedisConfig.java
@@ -12,7 +12,6 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@Profile("prod")
 public class RedisConfig {
 
 	@Value("${redis-session-host}")
