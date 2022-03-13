2521b0a380e07bb43a2197c7be289f9f2bfac8b1
wanniDev <wannidev0928@gmail.com>
2022-03-13 21:24:00 +0900

    Feat: objectMapper 설정 추가
    - java time module
    - null 값은 바인딩x

--- src/main/java/com/project/doubleshop/domain/common/config/AppConfig.java
+++ src/main/java/com/project/doubleshop/domain/common/config/AppConfig.java
@@ -1,5 +1,8 @@
 package com.project.doubleshop.domain.common.config;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -7,6 +10,15 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -29,5 +41,25 @@ public class AppConfig {
 			.username(dataSourceUsername)
 			.password(dataSourcePassword)
 			.build();
+	}
+
+	@Bean
+	public Jackson2ObjectMapperBuilder configureObjectMapper() {
+		// Java time module
+		JavaTimeModule jtm = new JavaTimeModule();
+		jtm.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ISO_DATE_TIME));
+		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder() {
+			@Override
+			public void configure(ObjectMapper objectMapper) {
+				super.configure(objectMapper);
+				objectMapper.setVisibility(PropertyAccessor.GETTER, JsonAutoDetect.Visibility.NONE);
+				objectMapper.setVisibility(PropertyAccessor.IS_GETTER, JsonAutoDetect.Visibility.NONE);
+				objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+			}
+		};
+		builder.serializationInclusion(JsonInclude.Include.NON_NULL);
+		builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		builder.modulesToInstall(jtm);
+		return builder;
 	}
 }
