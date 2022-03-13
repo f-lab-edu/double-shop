760d5004b0e45de78c8a174af959ad2e49c74cbe
wanniDev <wannidev0928@gmail.com>
2022-03-13 23:39:54 +0900

    Test: 배송, 배송 정책 단위 테스트 작성

--- src/test/java/com/project/doubleshop/domain/delivery/service/DeliveryPolicyServiceTest.java
+++ src/test/java/com/project/doubleshop/domain/delivery/service/DeliveryPolicyServiceTest.java
@@ -5,18 +5,22 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import com.project.doubleshop.domain.delivery.entity.DeliveryPolicy;
-import com.project.doubleshop.domain.delivery.repository.legacy.DeliveryPolicyRepository;
-import com.project.doubleshop.domain.delivery.service.DeliveryPolicyService;
+import com.project.doubleshop.domain.delivery.repository.DeliveryPolicyRepository;
 import com.project.doubleshop.domain.exception.NotFoundException;
-import com.project.doubleshop.web.config.support.Pageable;
 
 @ExtendWith(MockitoExtension.class)
 class DeliveryPolicyServiceTest {
@@ -24,16 +28,13 @@ class DeliveryPolicyServiceTest {
 	@Mock
 	DeliveryPolicyRepository deliveryPolicyRepository;
 
-	@Mock
-	Pageable pageable;
-
 	@InjectMocks
 	DeliveryPolicyService deliveryPolicyService;
 
 	@Test
 	@DisplayName("배송정책 등록 성공")
 	void saveDeliveryPolicy() {
-		given(deliveryPolicyRepository.save(DELIVERY_POLICY_FORM)).willReturn(true);
+		given(deliveryPolicyRepository.save(DELIVERY_POLICY_FORM)).willReturn(DELIVERY_POLICY_1);
 
 		DeliveryPolicy result = deliveryPolicyService.save(DELIVERY_POLICY_FORM);
 
@@ -43,7 +44,7 @@ class DeliveryPolicyServiceTest {
 	@Test
 	@DisplayName("배송정책 단건 조회 성공")
 	void findOneDeliveryPolicy() {
-		given(deliveryPolicyRepository.findById(ID)).willReturn(DELIVERY_POLICY_1);
+		given(deliveryPolicyRepository.findById(ID)).willReturn(Optional.of(DELIVERY_POLICY_1));
 
 		DeliveryPolicy result = deliveryPolicyService.findById(ID);
 
@@ -53,7 +54,7 @@ class DeliveryPolicyServiceTest {
 	@Test
 	@DisplayName("배송정책 조회 실패")
 	void findOneDeliveryPolicyFail() {
-		given(deliveryPolicyRepository.findById(ID)).willReturn(null);
+		given(deliveryPolicyRepository.findById(ID)).willReturn(Optional.ofNullable(null));
 
 		assertThrows(NotFoundException.class, () -> deliveryPolicyService.findById(ID));
 	}
@@ -61,21 +62,10 @@ class DeliveryPolicyServiceTest {
 	@Test
 	@DisplayName("배송정책 수정 성공")
 	void updateDeliveryPolicy() {
-		// given(deliveryPolicyRepository.findById(ID)).willReturn(DELIVERY_POLICY_1);
-		// given(deliveryPolicyRepository.save(DELIVERY_POLICY_1)).willReturn(true);
-		//
-		// deliveryPolicyService.saveDeliveryPolicy(DELIVERY_POLICY_1, ID);
-		//
-		// then(deliveryPolicyRepository).should(times(1)).save(DELIVERY_POLICY_1);
-	}
+		given(deliveryPolicyRepository.findById(ID)).willReturn(Optional.of(DELIVERY_POLICY_1));
 
-	@Test
-	@DisplayName("배송정책 목록 조회 성공")
-	void findAllDeliveryPolicies() {
-		// given(deliveryPolicyRepository.findAll(pageable)).willReturn(anyList());
-		//
-		// deliveryPolicyService.findDeliveryPolicies(pageable);
-		//
-		// then(deliveryPolicyRepository).should(times(1)).findAll(pageable);
+		deliveryPolicyService.updateDeliveryPolicy(ID, DELIVERY_POLICY_1);
+
+		then(deliveryPolicyRepository).should(times(1)).save(DELIVERY_POLICY_1);
 	}
 }
