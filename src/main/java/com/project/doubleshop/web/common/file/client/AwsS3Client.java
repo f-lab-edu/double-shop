347e8d05d9c2fe2348d6c18555a87e177868a178
wanniDev <wannidev0928@gmail.com>
2022-03-24 01:30:30 +0900

    Feat: 프로파일  정리

--- src/main/java/com/project/doubleshop/web/common/file/client/AwsS3Client.java
+++ src/main/java/com/project/doubleshop/web/common/file/client/AwsS3Client.java
@@ -27,7 +27,7 @@ import com.amazonaws.services.s3.model.S3ObjectSummary;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@Profile("local")
+@Profile("prod")
 @Slf4j
 @Component
 @RequiredArgsConstructor
