45e1a15cbe881feeda8b71de8c23bdf8bb95fc5c
wanniDev <wannidev0928@gmail.com>
2022-03-24 13:16:49 +0900

    Feat: async 설정 및 sync io, async io 구현

--- src/main/java/com/project/doubleshop/web/common/file/client/DefaultFileClient.java
+++ src/main/java/com/project/doubleshop/web/common/file/client/DefaultFileClient.java
@@ -8,16 +8,21 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.Future;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Component;
 import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Profile("local")
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class DefaultFileClient implements FileClient {
 
@@ -57,8 +62,22 @@ public class DefaultFileClient implements FileClient {
 	public String upload(InputStream inputStream, long length, String key, String contentType, Map<String, String> metadata) {
 		try {
 			Files.copy(inputStream, this.root.resolve(Objects.requireNonNull(key)));
-			Thread.sleep(500L);
+			Thread.sleep(500);
 			return this.root + File.separator + key;
+		} catch (IOException | InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	@Async
+	public Future<String> uploadAsync(InputStream inputStream, long length, String key, String contentType,
+		Map<String, String> metadata) {
+		try {
+			log.info(Thread.currentThread().getName());
+			Files.copy(inputStream, this.root.resolve(Objects.requireNonNull(key)));
+			Thread.sleep(500);
+			return new AsyncResult<>(this.root + File.separator + key);
 		} catch (IOException | InterruptedException e) {
 			throw new RuntimeException(e);
 		}
