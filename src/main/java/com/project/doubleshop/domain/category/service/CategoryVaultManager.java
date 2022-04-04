959a771b114a5ea209ea31c3d69892bd1d7eaf51
wanniDev <wannidev0928@gmail.com>
2022-04-04 23:01:13 +0900

    Feat: 카테고리 캐싱 마무리

---
+++ src/main/java/com/project/doubleshop/domain/category/service/CategoryVaultManager.java
@@ -0,0 +1,54 @@
+package com.project.doubleshop.domain.category.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.stereotype.Component;
+
+import com.project.doubleshop.domain.category.entity.Category;
+import com.project.doubleshop.domain.category.repository.CategoryRepository;
+import com.project.doubleshop.domain.category.service.CategoryService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class CategoryVaultManager {
+
+	private final CategoryRepository categoryService;
+
+	private final Cache cache;
+
+	private final List<Category> cachedCategories;
+
+	public CategoryVaultManager(CategoryRepository categoryService, @Qualifier("categoryCache") Cache cache) {
+		this.categoryService = categoryService;
+		this.cache = cache;
+		this.cachedCategories = new ArrayList<>();
+	}
+
+	@PostConstruct
+	private void warmUp() {
+		List<Category> categories = categoryService.findAll();
+		categories.forEach(c -> {
+			cache.put(c.getId(), c);
+			cachedCategories.add(c);
+		});
+		log.info("All Categories cached successful");
+	}
+
+	public Category findCachedCategory(Long categoryId) {
+		return cache.get(categoryId, Category.class);
+	}
+
+	public List<Category> findCachedCategories() {
+		return this.cachedCategories;
+	}
+}
