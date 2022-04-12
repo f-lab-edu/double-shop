cad78a613e7763885d68ba68c0be166743ca788b
wanniDev <wannidev0928@gmail.com>
2022-04-12 20:22:11 +0900

    Feat: Redis 캐시 설정

--- src/main/java/com/project/doubleshop/DoubleShopApplication.java
+++ src/main/java/com/project/doubleshop/DoubleShopApplication.java
@@ -2,8 +2,10 @@ package com.project.doubleshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class DoubleShopApplication {
 
     public static void main(String[] args) {
