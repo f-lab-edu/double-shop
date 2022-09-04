7bc544bdbe313f531d5cefa41b9bed11df8e6a79
wanniDev <wannidev0928@gmail.com>
2022-09-04 12:16:23 +0900

    Test: 대소문자, 숫자, 특수문자 조합을 해야하는 정규식으로 변경 후 테스트에 적용

--- src/test/java/com/project/doubleshop/domain/member/service/JoinMemberTest.java
+++ src/test/java/com/project/doubleshop/domain/member/service/JoinMemberTest.java
@@ -5,7 +5,15 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.*;
-import static org.mockito.Mockito.times;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +23,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.project.doubleshop.domain.exception.DuplicateMemberException;
 import com.project.doubleshop.domain.member.entity.Member;
 import com.project.doubleshop.domain.member.repository.MemberRepository;
 import com.project.doubleshop.web.member.dto.JoinRequest;
@@ -22,7 +31,7 @@ import com.project.doubleshop.web.member.dto.JoinRequest;
 @ExtendWith(MockitoExtension.class)
 public class JoinMemberTest {
 	@Mock
-	MemberRepository authMemberRepository;
+	MemberRepository memberRepository;
 
 	@Mock
 	PasswordEncoder passwordEncoder;
@@ -35,16 +44,36 @@ public class JoinMemberTest {
 	void validMemberJoinTest() {
 		JoinRequest joinRequest = new JoinRequest(USER_ID, PASSWORD, NAME, EMAIL, PHONE);
 
-		when(authMemberRepository.save(any(Member.class))).thenReturn(MEMBER);
+		when(memberRepository.findByUserId(USER_ID)).thenReturn(Optional.empty());
+		when(memberRepository.save(any(Member.class))).thenReturn(MEMBER);
 
 		Member member = authMemberService.join(joinRequest);
+		Boolean isExists = authMemberService.isExists(Map.of("userId", USER_ID));
 
+		assertThat(isExists).isFalse();
 		assertThat(MEMBER.getId()).isEqualTo(member.getId());
 	}
-	
-	@Test
-	@DisplayName("회원가입 양식에 맞지 않게 입력한 경우, 회원가입 실패")
-	void invalidMemberJoinTest() {
 
+	@Test
+	@DisplayName("이미 존재하는 아이디로 회원가입을 하려고 한다면, 회원가입은 실패한다.")
+	void invalidMemberJoinTest() {
+		JoinRequest joinRequest = new JoinRequest(USER_ID, PASSWORD, NAME, EMAIL, PHONE);
+
+		when(memberRepository.findByUserId(USER_ID)).thenReturn(Optional.ofNullable(MEMBER));
+
+		assertThrows(DuplicateMemberException.class, () -> authMemberService.join(joinRequest));
+	}
+
+	@Test
+	@DisplayName("회원가입 양식에 맞지 않게 입력한 경우, validation 에 위반하는 메세지를 받는다.")
+	void memberJoinValidationTest() {
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		Validator validator = factory.getValidator();
+
+		JoinRequest joinRequest = new JoinRequest(USER_ID, "qwe", NAME, EMAIL, PHONE);
+
+		Set<ConstraintViolation<JoinRequest>> violations = validator.validate(joinRequest);
+
+		assertThat(violations).isNotEmpty();
 	}
 }
