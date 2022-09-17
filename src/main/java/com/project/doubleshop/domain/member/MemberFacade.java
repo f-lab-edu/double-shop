34d7c28edd36100320bba1bb92867ab6140138b2
wanniDev <wannidev0928@gmail.com>
2022-09-17 23:06:20 +0900

    Refactor: Facade 패턴을 활용하여, AuthController에 인증 관련 비즈니스 로직을 단계별로 추상화

--- src/main/java/com/project/doubleshop/domain/member/MemberFacade.java
+++ src/main/java/com/project/doubleshop/domain/member/MemberFacade.java
@@ -1,15 +1,33 @@
 package com.project.doubleshop.domain.member;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import com.project.doubleshop.domain.exception.UnauthenticatedMemberException;
+import com.project.doubleshop.domain.member.infrastructure.token.SimpleAuthenticationToken;
+import com.project.doubleshop.web.member.dto.AuthenticationResult;
+import com.project.doubleshop.web.member.dto.AuthenticationResultDto;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class MemberFacade {
-	private final MemberAuthProcessor memberAuthProcessor;
+	private final AuthenticationManager authenticationManager;
 
-	public void login(String userId, String password) {
-		memberAuthProcessor.login(userId, password);
+	public AuthenticationResultDto login(String userId, String password) {
+		try {
+			SimpleAuthenticationToken authToken = new SimpleAuthenticationToken(
+				userId, password);
+			Authentication authentication = authenticationManager.authenticate(authToken);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+			return new AuthenticationResultDto((AuthenticationResult) authentication.getDetails());
+		} catch (AuthenticationException e) {
+			throw new UnauthenticatedMemberException(e.getMessage(), HttpStatus.UNAUTHORIZED.value());
+		}
 	}
 }
