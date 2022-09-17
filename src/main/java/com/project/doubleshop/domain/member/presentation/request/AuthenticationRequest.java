5eb9c07d10c4c74f958c3d0a3548089b47501411
wanniDev <wannidev0928@gmail.com>
2022-09-18 02:00:06 +0900

    Refactor: 인증 컨트롤러 프레젠테이션 계층에 속한 패키지로 재분류

---
+++ src/main/java/com/project/doubleshop/domain/member/presentation/request/AuthenticationRequest.java
@@ -0,0 +1,24 @@
+package com.project.doubleshop.domain.member.presentation.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+public class AuthenticationRequest {
+
+	private String principal;
+
+	private String credential;
+
+	@Override
+	public String toString() {
+		return "AuthenticationRequest{" +
+			"principal='" + principal + '\'' +
+			", credential='" + credential + '\'' +
+			'}';
+	}
+}
