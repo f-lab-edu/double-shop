58d2b4e746dfb05a4619fddf34be91b81d99ceff
wanniDev <wannidev0928@gmail.com>
2022-10-13 19:22:16 +0900

    Refactor: 상품 등록 api 네이밍 수정

--- src/main/java/com/project/doubleshop/item/infrastructure/ItemCreator.java
+++ src/main/java/com/project/doubleshop/item/infrastructure/ItemCreator.java
@@ -2,7 +2,6 @@ package com.project.doubleshop.item.infrastructure;
 
 import static com.project.doubleshop.web.common.file.ImageFile.*;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.project.doubleshop.item.domain.Item;
@@ -13,11 +12,11 @@ import com.project.doubleshop.web.item.dto.ItemForm;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class ItemRegister {
+public class ItemCreator {
 	private final FileClient fileClient;
 	private final ItemRepository itemRepository;
 
-	public Item registerWith(ItemForm itemForm, MultipartFile imageFile) {
+	public Item createWith(ItemForm itemForm, MultipartFile imageFile) {
 		String imageUrl = uploadAsyncImageFile(fileClient, of(imageFile), null);
 		Item item = itemRepository.save(Item.convertToItem(itemForm));
 		item.setImageUri(imageUrl);
